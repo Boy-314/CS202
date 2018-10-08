9cd9feaf1bf03b2c3ba3218eb17f41672ec40869
Boy-314 <boy.314.yee.willie@gmail.com>
2018-10-07 21:40:38 -0400

    october 7 2018

--- lab2/lab2.cc
+++ lab2/lab2.cc
@@ -113,11 +113,11 @@ void verboseOutput(int cycle, vector<process> p)
 		}
 		if(p[i].status == "running")
 		{
-			cout << setw(3) << p[i].cpuBurstTimeLeft + 1 << setw(5);
+			cout << setw(3) << p[i].cpuBurstTimeLeft << setw(5);
 		}
 		if(p[i].status == "blocked")
 		{
-			cout << setw(3) << p[i].ioBurst + 1 << setw(5);
+			cout << setw(3) << p[i].ioBurst << setw(5);
 		}
 		if(p[i].status == "terminated")
 		{
@@ -132,15 +132,68 @@ void FCFS(vector<process> pVector)
 	int totalFinishedProcesses = 0;
 	int cycle = 0;
 	bool busy = false;
+	vector<process> blocked;
 	queue<process> readyQ;
+	
+	/*
+	while not all processes are finished
+		iterate through blocked array
+			if process in blocked array finished io burst
+				push it onto the ready queue, remove it from the blocked vector
+			else
+				decrement ioBurst and increment ioTotalTime
+		iterate through all processes
+			if process status is unstarted and process has already arrived
+				set status to ready, push on to ready queue, increment waiting time
+			if cpu is not busy
+				if process at the front of the ready queue has arrived
+					set process status to running, pop it off the ready queue, and set cpu to busy
+			if process is running
+				if process runs out of cpu burst time
+					add it to the blocked vector, set status to blocked, change cpu to not busy
+				else
+					decrement cpu burst time left, and C
+			if process finishes total cpu time
+				set status to terminated and increment total finished processes
+		cycle++
+	*/
 	while(totalFinishedProcesses != pVector.size())
 	{
+		// if verbose mode is active, print out verbose information
 		if(verbose == true)
 		{
 			verboseOutput(cycle,pVector);
 		}
-		for(int i = 0; i < pVector.size(); i++)
+		
+		// check blocked vector to see if any processes can be moved to the ready queue
+		for(int j = 0; j < blocked.size(); j++)
 		{
+			// find corresponding element in pVector
+			int index = -1;
+			for(int k = 0; k < pVector.size(); k++)
+			{
+				if(blocked[j].order == pVector[k].order)
+				{
+					index = k;
+				}
+			}
+						
+			// -1 ioBurst for all processes in blocked array
+			pVector[index].ioBurst--;
+			pVector[index].ioTotalTime++;
+			
+			// if process in blocked array has finished io burst
+			if(pVector[index].ioBurst <= 0)
+			{
+				readyQ.push(pVector[index]);
+				pVector[index].status = "ready";
+				blocked.erase(blocked.begin() + index);
+			}
+		}
+		
+		// iterate through processes
+		for(int i = 0; i < pVector.size(); i++)
+		{	
 			// if process is unstarted and has arrived
 			if(pVector[i].status == "unstarted" && pVector[i].A <= cycle)
 			{
@@ -149,14 +202,14 @@ void FCFS(vector<process> pVector)
 				pVector[i].waitingTime++;
 			}
 			
-			// if process at the top of the queue has arrival time less than cycle, and cpu is not busy
-			if(readyQ.front().A < cycle && !busy)
+			// if cpu is not busy
+			if(!busy && !readyQ.empty())
 			{
 				process temp = readyQ.front();
-				int index;
+				int index = -1;
 				for(int j = 0; j < pVector.size(); j++)
 				{
-					if(pVector[i].order == temp.order)
+					if(pVector[j].order == temp.order)
 					{
 						index = j;
 					}
@@ -166,57 +219,49 @@ void FCFS(vector<process> pVector)
 				busy = true;
 			}
 			
-			// if process is running and still has cpu burst time
-			if(pVector[i].status == "running" && pVector[i].cpuBurstTimeLeft > 0)
+			cout << "\nstatus: " << pVector[0].status << endl;
+			cout << "burst left: " << pVector[0].cpuBurstTimeLeft << endl;
+			
+			// if process is running
+			if(pVector[i].status == "running")
 			{
-				pVector[i].cpuBurstTimeLeft--;
-				pVector[i].C--;
-				busy = true;
+				// if process runs out of cpu burst time
+				if(pVector[i].cpuBurstTimeLeft <= 0)
+				{
+					blocked.push_back(pVector[i]);
+					pVector[i].status = "blocked";
+					pVector[i].cpuBurstTimeLeft = pVector[i].cpuBurst;
+					busy = false;
+				}
+				
+				// if process still has cpu burst time
+				else
+				{
+					cout << "working\n";
+					pVector[i].cpuBurstTimeLeft--;
+					pVector[i].C--;
+				}
 			}
 			
-			// if process is running but cpu burst runs out
-			else if(pVector[i].status == "running" && pVector[i].cpuBurstTimeLeft <= 0)
-			{
-				pVector[i].status = "blocked";
-				busy = false;
-			}
-			
-			// if process is running but completes all cpu time
-			else if(pVector[i].status == "running" && pVector[i].C <= 0)
+			// if process finishes total cpu time
+			if(pVector[i].C <= 0)
 			{
 				pVector[i].status = "terminated";
-				totalFinishedProcesses++;
 				pVector[i].finishTime = cycle;
-				pVector[i].turnaroundTime = pVector[i].finishTime - pVector[i].A;
-				busy = false;
+				pVector[i].turnaroundTime = cycle - pVector[i].A;
+				totalFinishedProcesses++;
 			}
 			
-			// if process is blocked and still has io burst time
-			if(pVector[i].status == "blocked" && pVector[i].ioBurst > 0)
+			for(int i = 0; i < pVector.size(); i++)
 			{
-				pVector[i].ioBurst--;
-				pVector[i].ioTotalTime++;
-			}
-			
-			// if process is blocked, but finished io, and cpu is not busy with another process
-			else if(pVector[i].status == "blocked" && pVector[i].ioBurst <= 0 && !busy)
-			{
-				pVector[i].status = "running";
-				pVector[i].C--;
-				pVector[i].cpuBurstTimeLeft = pVector[i].cpuBurst;
-				pVector[i].cpuBurstTimeLeft--;
-				busy = true;
-			}
-			
-			// if process is blocked, but finished io, but cpu is busy with another process
-			else if(pVector[i].status == "blocked" && pVector[i].ioBurst <= 0 && busy)
-			{
-				pVector[i].status = "ready";
-				readyQ.push(pVector[i]);
-				pVector[i].waitingTime++;
+				cout << endl << "i: " << i << ", " << pVector[i].status << ", " << "cpu: " << pVector[i].cpuBurstTimeLeft << ", " << "io: " << pVector[i].ioBurst << endl;
 			}
 		}
 		cycle++;
+		if(cycle > 25)
+		{
+			break;
+		}
 	}
 }
 
@@ -248,7 +293,23 @@ int main(int argc, char ** argv)
 	for(int i = 0; i < NumOfProcesses; i++)
 	{
 		int A,B,C,M;
-		inputFile >> A >> B >> C >> M;
+		inputFile >> A >> B >> C >> M;	
+		/*
+		int order;
+		int A;
+		int B;
+		int C;
+		int M;
+		int finishTime;
+		int turnaroundTime;
+		int ioBurst;
+		int ioTotalTime;
+		int waitingTime;
+		int cpuBurst;
+		int cpuBurstTimeLeft;
+		int quantum;
+		string status;
+		*/
 		processesVector.push_back({i,A,B,C,M,0,0,randomOS(B) * M,0,0,randomOS(B),randomOS(B),2,"unstarted"});
 	}
 	
