8743373cc5f5c5d072aee0a6f61bac6847792e63
Boy-314 <boy.314.yee.willie@gmail.com>
2018-11-17 12:46:08 -0500

    created activity struct and handled user/file input

---
+++ lab2/readme.md
@@ -0,0 +1,9 @@
+# HOW TO COMPILE AND RUN
+
+To compile, make sure your current directiry contains the source code file, and run `g++ -std=c++11 lab2.cc -o NAME`
+
+To run normally, execute `./NAME FILENAME`
+
+To run with verbose output, execute `./NAME --verbose FILENAME`
+
+NOTE: THIS LAB DOES NOT HANDLE FILENOTFOUND EXCEPTIONS
--- lab3/lab3.cc
+++ lab3/lab3.cc
@@ -12,7 +12,67 @@
 #include <vector>
 using namespace std;
 
-int main()
+/* 
+ * structure for each activity, as seen in the inputs
+ * command: initiate, request, compute, release, or terminate
+ * one, two, three: unsigned integers that are needed as parameters for each command
+ */
+struct activity
 {
+	string command;
+	int one;
+	int two;
+	int three;
+};
+
+// optimistic resource manager
+void optimistic(int num_of_tasks, int num_of_rtypes, vector<int> units, vector<activity> activities)
+{
+}
+
+// banker's algorithm
+void banker(int num_of_tasks, int num_of_rtypes, vector<int> units, vector<activity> activities)
+{
+}
+
+int main(int argc, char * argv[])
+{
+	int num_of_tasks;
+	int num_of_rtypes;
+	vector<int> units;
+	vector<activity> activities;
+	
+	// if user did not include a filename, output an error
+	if(argc == 1)
+	{
+		cout << "error: missing command line argument FILENAME\n";
+	}
+	
+	else if(argc == 2)
+	{
+		ifstream input;
+		input.open(argv[1]);
+		
+		// save the number of tasks and the number of resource types
+		input >> num_of_tasks >> num_of_rtypes;
+		
+		for(int i = 0; i < num_of_rtypes; i++)
+		{
+			int unit;
+			input >> unit;
+			
+			// save the number of units for each resource type
+			units.push_back(unit);
+		}
+		
+		// push back activities into the vector of activities
+		string command;
+		int one, two, three;
+		while(input >> command >> one >> two >> three)
+		{
+			activity a = {command,one,two,three};
+			activities.push_back(a);
+		}
+	}
 	return 0;
 }
