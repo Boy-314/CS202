77b96e620433e97ffc839804fb9b5310b39a04bc
Boy-314 <boy.314.yee.willie@gmail.com>
2018-11-18 17:28:54 -0500

    november 18 2018

--- lab3/lab3.cc
+++ lab3/lab3.cc
@@ -23,22 +23,100 @@ struct activity
 	int one;
 	int two;
 	int three;
+	string status;
+};
+
+struct task
+{
+	int task_number;
+	int resource_type;
+	int busy_cycles;
+	string status;
+	int initial_claim;
+	int time_taken;
+	int waiting_time;
+	double percent_waiting;
 };
 
 // optimistic resource manager
-void optimistic(int num_of_tasks, int num_of_rtypes, vector<int> units, vector<activity> activities)
+void optimistic(int T, int R, vector<int> units, vector<activity> activities)
 {
+	int cycle = 0;
+	int tasks_completed = 0;
+	
+	vector<task> tasks;
+	queue<activity> requests;
+	
+	for(int i = 0; i < T; i++)
+	{
+		task t = {i + 1,0,0,"unstarted",0,0,0,0};
+		tasks.push_back(t);
+	}
+	
+	while(tasks_completed != T)
+	{
+		// initialize each task
+		for(auto i : activities)
+		{
+			if(i.command == "initiate")
+			{
+				int t_num = i.one;
+				if(tasks[t_num - 1].status != "initialized")
+				{
+					tasks[t_num - 1].status = "initialized";
+					cout << "initializing " << tasks[t_num - 1].task_number << endl;
+				}
+			}
+		}
+		cycle++;
+		
+		// put requests into queue
+		for(auto i : activities)
+		{
+			if(i.command == "request")
+			{
+				requests.push(i);
+			}
+		}
+		
+		// grant requests
+		// queue that holds all requests that cannot be granted this cycle
+		queue<activity> ungranted_requests;
+		while(!requests.empty())
+		{
+			int units_avail = units[requests.front().one - 1];
+			int res_type = requests.front().two;
+			int units_requested = requests.front().three;
+			if(units_avail - units_requested >= 0)
+			{
+				tasks[requests.front().one - 1].resource_type = res_type;
+				tasks[requests.front().one - 1].status = "request granted";
+				units[requests.front().one - 1] = units[requests.front().one - 1] - units_requested;
+				requests.pop();
+			}
+			else
+			{
+				ungranted_requests.push(requests.front());
+				requests.pop();
+			}
+		}
+		requests = ungranted_requests;
+		cycle++;
+		
+		// release resources
+		
+	}
 }
 
 // banker's algorithm
-void banker(int num_of_tasks, int num_of_rtypes, vector<int> units, vector<activity> activities)
+void banker(int T, int R, vector<int> units, vector<activity> activities)
 {
 }
 
 int main(int argc, char * argv[])
 {
-	int num_of_tasks;
-	int num_of_rtypes;
+	int T;
+	int R;
 	vector<int> units;
 	vector<activity> activities;
 	
@@ -54,9 +132,9 @@ int main(int argc, char * argv[])
 		input.open(argv[1]);
 		
 		// save the number of tasks and the number of resource types
-		input >> num_of_tasks >> num_of_rtypes;
+		input >> T >> R;
 		
-		for(int i = 0; i < num_of_rtypes; i++)
+		for(int i = 0; i < R; i++)
 		{
 			int unit;
 			input >> unit;
@@ -70,9 +148,12 @@ int main(int argc, char * argv[])
 		int one, two, three;
 		while(input >> command >> one >> two >> three)
 		{
-			activity a = {command,one,two,three};
+			activity a = {command,one,two,three,"unstarted"};
 			activities.push_back(a);
 		}
 	}
+	
+	optimistic(T,R,units,activities);
+	
 	return 0;
 }
