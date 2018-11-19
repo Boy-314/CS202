a3e8ae580909608c51c6f203553833401a8c2ab2
Boy-314 <boy.314.yee.willie@gmail.com>
2018-11-18 22:20:13 -0500

    TODO: fix push into requests queue

--- lab3/lab3.cc
+++ lab3/lab3.cc
@@ -32,11 +32,30 @@ struct task
 	int resource_type;
 	int busy_cycles;
 	string status;
+	vector<activity> activities;
 	int initial_claim;
 	int time_taken;
 	int waiting_time;
 	double percent_waiting;
 };
+
+// method to print final output
+void print_output(vector<task> tasks, string resource_manager)
+{
+	cout << setw(19) << resource_manager << endl;
+	int total_time_taken = 0;
+	int total_waiting_time = 0;
+	double total_percent_waiting = 0;
+	for(auto i : tasks)
+	{
+		cout << setw(11) << "Task " << i.task_number << setw(7) << i.time_taken << setw(4) << i.waiting_time << setw(4) << i.percent_waiting << "%" << endl;
+		total_time_taken = total_time_taken + i.time_taken;
+		total_waiting_time = total_waiting_time + i.waiting_time;
+		total_percent_waiting = total_percent_waiting + i.waiting_time;
+	}
+	total_percent_waiting = 100.0 * total_percent_waiting / total_time_taken;
+	cout << setw(11) << "total" << setw(8) << total_time_taken << setw(4) << total_waiting_time << setw(4) << total_percent_waiting << "%";
+}
 
 // optimistic resource manager
 void optimistic(int T, int R, vector<int> units, vector<activity> activities)
@@ -47,9 +66,19 @@ void optimistic(int T, int R, vector<int> units, vector<activity> activities)
 	vector<task> tasks;
 	queue<activity> requests;
 	
+	int total_requests = 0;
 	for(int i = 0; i < T; i++)
 	{
-		task t = {i + 1,0,0,"unstarted",0,0,0,0};
+		vector<activity> task_activity;
+		for(auto j : activities)
+		{
+			if(j.one == i + 1 && j.command == "request")
+			{
+				task_activity.push_back(j);
+				total_requests++;
+			}
+		}
+		task t = {i + 1,0,0,"unstarted",task_activity,0,0,0,0};
 		tasks.push_back(t);
 	}
 	
@@ -63,19 +92,30 @@ void optimistic(int T, int R, vector<int> units, vector<activity> activities)
 				int t_num = i.one;
 				if(tasks[t_num - 1].status != "initialized")
 				{
-					tasks[t_num - 1].status = "initialized";
 					cout << "initializing " << tasks[t_num - 1].task_number << endl;
+					tasks[t_num - 1].status = "initialized";
 				}
 			}
 		}
 		cycle++;
 		
 		// put requests into queue
-		for(auto i : activities)
+		int pushed = 0;
+		while(pushed != total_requests)
 		{
-			if(i.command == "request")
+			for(auto i : activities)
 			{
-				requests.push(i);
+				for(int j = 0; j < T; j++)
+				{
+					cout << i.command << "," << i.one << "," << j + 1 << endl;
+					if(i.command == "request" && i.one == j + 1)
+					{
+						cout << "pushing to request queue: " << i.one << "," << i.two << "," << i.three << endl;
+						requests.push(i);
+						i.status = "accounted";
+						pushed++;
+					}
+				}
 			}
 		}
 		
@@ -84,19 +124,24 @@ void optimistic(int T, int R, vector<int> units, vector<activity> activities)
 		queue<activity> ungranted_requests;
 		while(!requests.empty())
 		{
-			int units_avail = units[requests.front().one - 1];
+			int units_avail = units[requests.front().two - 1];
 			int res_type = requests.front().two;
 			int units_requested = requests.front().three;
+			cout << units_avail << "," << units_requested << endl;
 			if(units_avail - units_requested >= 0)
 			{
+				cout << "granting request: " << requests.front().one << ", " << res_type << ", " << units_requested << endl;
 				tasks[requests.front().one - 1].resource_type = res_type;
 				tasks[requests.front().one - 1].status = "request granted";
-				units[requests.front().one - 1] = units[requests.front().one - 1] - units_requested;
+				units[requests.front().two - 1] = units[requests.front().two - 1] - units_requested;
+				cout << "units left: " << units[requests.front().two - 1] << endl;
 				requests.pop();
 			}
 			else
 			{
+				cout << "cannot grant request\n";
 				ungranted_requests.push(requests.front());
+				tasks[requests.front().one - 1].waiting_time++;
 				requests.pop();
 			}
 		}
@@ -104,8 +149,33 @@ void optimistic(int T, int R, vector<int> units, vector<activity> activities)
 		cycle++;
 		
 		// release resources
+		for(auto i : activities)
+		{
+			if(i.command == "release" && tasks[i.one - 1].status == "request granted")
+			{
+				cout << "releasing " << i.one << endl;
+				tasks[i.one - 1].status == "released";
+				units[i.two - 1] = units[i.two - 1] + i.three;
+				cout << "units left: " << units[i.two - 1] << endl;
+			}
+		}
+		cycle++;
 		
+		// handle terminates
+		for(auto i : activities)
+		{
+			if(i.command == "terminate")
+			{
+				cout << "terminating " << i.one - 1 << endl;
+				tasks[i.one - 1].status = "terminated";
+				tasks[i.one - 1].time_taken = cycle;
+				tasks[i.one - 1].percent_waiting = 100.0 * tasks[i.one - 1].waiting_time / tasks[i.one - 1].time_taken;
+				tasks_completed++;
+			}
+		}
 	}
+	
+	print_output(tasks, "FIFO");
 }
 
 // banker's algorithm
