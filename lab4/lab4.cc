a77d1a92a59d67946758b75ad6d066712bb999f3
Boy-314 <boy.314.yee.willie@gmail.com>
2018-12-03 14:25:14 -0500

    added easy output

--- lab4/lab4.cc
+++ lab4/lab4.cc
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstdlib>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -20,6 +21,23 @@ int main(int argc, char * argv[])
 	// J, the ‘‘job mix’’, which determines A, B, and C, as described below.
 	// N, the number of references for each process.
 	// R, the replacement algorithm, LIFO (NOT FIFO), RANDOM, or LRU.
-	int M = argv[1];
+	int M = atoi(argv[1]);
+	int P = atoi(argv[2]);
+	int S = atoi(argv[3]);
+	int J = atoi(argv[4]);
+	int N = atoi(argv[5]);
+	string R = argv[6];
+	
+	// output the command line arguments
+	cout << "The machine size is " << M << "." << endl;
+	cout << "The page size is " << P << "." << endl;
+	cout << "The process size is " << S << "." << endl;
+	cout << "The job mix number is " << J << "." << endl;
+	cout << "The number of references per process is " << N << "." << endl;
+	cout << "The replacement algorithm is " << R << "." << endl;
+	
+	// handle job mix argument
+	
+	
 	return 0;
 }
