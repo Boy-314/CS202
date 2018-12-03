a9ca5b98a6f10d143a231ec344bf37072d26b4e9
Boy-314 <boy.314.yee.willie@gmail.com>
2018-12-03 16:16:08 -0500

    implemented probabilities

--- lab4/lab4.cc
+++ lab4/lab4.cc
@@ -37,7 +37,99 @@ int main(int argc, char * argv[])
 	cout << "The replacement algorithm is " << R << "." << endl;
 	
 	// handle job mix argument
+	vector<vector<double>> probabilities;		// vector of tasks and their probabilities
+	double num_of_processes;
 	
+	// A,B,C,D: as seen in the lab pdf file
+	double A = 0;
+	double B = 1;
+	double C = 2;
+	double D = 3;
+	
+	// One process, A=1, B=C=0
+	if(J == 1)
+	{
+		num_of_processes = 1;
+		vector<double> temp;
+		probabilities.push_back(temp);
+		probabilities[0].push_back(1);
+		probabilities[0].push_back(0);
+		probabilities[0].push_back(0);
+		probabilities[0].push_back(0);
+	}
+	
+	// Four processes, each with A=1 and B=C=0
+	if(J == 2)
+	{
+		num_of_processes = 4;
+		for(double i = 0; i < num_of_processes; i++)
+		{
+			vector<double> temp;
+			probabilities.push_back(temp);
+			probabilities[i].push_back(1);
+			probabilities[i].push_back(0);
+			probabilities[i].push_back(0);
+			probabilities[i].push_back(0);
+		}
+	}
+	
+	// Four processes, each with A=B=C=0
+	if(J == 3)
+	{
+		num_of_processes = 4;
+		for(double i = 0; i < num_of_processes; i++)
+		{
+			vector<double> temp;
+			probabilities.push_back(temp);
+			probabilities[i].push_back(0);
+			probabilities[i].push_back(0);
+			probabilities[i].push_back(0);
+			probabilities[i].push_back(1);
+		}
+	}
+	
+	// One process with A=0.75, B=0.25, C=0
+	// One process with A=0.75, B=0, C=0.25
+	// One process with A=0.75, B=0.125, C=0.125
+	// One process with A=0.5, B=0.125, C=0.125
+	if(J == 4)
+	{
+		num_of_processes = 4;
+		for(int i = 0; i < num_of_processes; i++)
+		{
+			vector<double> temp;
+			probabilities.push_back(temp);
+		}
+		
+		probabilities[0].push_back(0.75);
+		probabilities[0].push_back(0.25);
+		probabilities[0].push_back(0);
+		probabilities[0].push_back(0);
+		
+		probabilities[1].push_back(0.75);
+		probabilities[1].push_back(0);
+		probabilities[1].push_back(0.25);
+		probabilities[1].push_back(0);
+		
+		probabilities[2].push_back(0.75);
+		probabilities[2].push_back(0.125);
+		probabilities[2].push_back(0.125);
+		probabilities[2].push_back(0);
+		
+		probabilities[3].push_back(0.5);
+		probabilities[3].push_back(0.125);
+		probabilities[3].push_back(0.125);
+		probabilities[3].push_back(0.25);
+	}
+	
+	// for(auto i : probabilities)
+	// {
+		// for(auto j : i)
+		// {
+			// cout << j << " ";
+		// }
+		// cout << endl;
+	// }
 	
 	return 0;
 }
