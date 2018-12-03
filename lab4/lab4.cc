969261b97a5d917c0afcf2864aaf8b9e37c4745c
Boy-314 <boy.314.yee.willie@gmail.com>
2018-12-03 11:52:11 -0500

    need to switch machines

--- lab4/lab4.cc
+++ lab4/lab4.cc
@@ -14,5 +14,12 @@ using namespace std;
 
 int main(int argc, char * argv[])
 {
+	// M, the machine size in words.
+	// P, the page size in words.
+	// S, the size of each process, i.e., the references are to virtual addresses 0..S-1.
+	// J, the ‘‘job mix’’, which determines A, B, and C, as described below.
+	// N, the number of references for each process.
+	// R, the replacement algorithm, LIFO (NOT FIFO), RANDOM, or LRU.
+	int M = argv[1];
 	return 0;
 }
